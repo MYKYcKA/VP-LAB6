db2cca8f0532d4526593d969fffc575367dcc947
MYKYcKA <79082403+MYKYcKA@users.noreply.github.com>
2022-06-02 08:31:56 +0300

    LAB 6

---
+++ Lab6/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        private Point MouseDownLocation;
+        public Polygon poly = new Polygon(new PointF[] {
+            new PointF(200, 300), 
+            new PointF(850, 300),
+            new PointF(400, 200), 
+            new PointF(700, 200), 
+            new PointF(400, 455), 
+            new PointF(700, 455) });
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Paint_Polygon(object sender, PaintEventArgs e)
+        {
+            poly.Draw(sender, e);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            MouseDownLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var vector2 = new Point(e.X - MouseDownLocation.X, e.Y - MouseDownLocation.Y);
+            var vector1 = new Point(0, 1);
+            double angleInRadians = Math.Atan2(vector2.Y, vector2.X) - Math.Atan2(vector1.Y, vector1.X);
+            double lent = Math.Sqrt(vector2.X*vector2.X + vector2.Y + vector2.Y);
+            if (e.Button == MouseButtons.Left && Polygon.isInside(poly.Vertex,poly.VerticeCount,MouseDownLocation))
+            {
+                poly.MoveBy((float)(e.X - MouseDownLocation.X), (float)(e.Y - MouseDownLocation.Y));
+                MouseDownLocation = e.Location;
+                this.Invalidate();
+            }
+            if (e.Button == MouseButtons.Right && lent > 10)
+            {
+                poly.Rotate((float)angleInRadians, new PointF(e.X, e.Y));
+                MouseDownLocation = e.Location;
+                this.Invalidate();
+            }
+        }
+    }
+}
---
+++ Lab6/Polygon.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Lab6
+{
+    public class Polygon
+    {
+        public PointF[] Vertex = null;
+        public int VerticeCount = 0;
+
+        public Polygon()
+        {
+            VerticeCount = 4;
+            Vertex = new PointF[VerticeCount];
+        }
+
+        public Polygon(params PointF[] points)
+        {
+            Vertex = points;
+            VerticeCount = points.Length;
+        }
+
+        public void Draw(object sender, PaintEventArgs e)
+        {
+            var gr = e.Graphics;
+            var p = new Pen(Color.BlueViolet, 5);
+            gr.DrawPolygon(p, Vertex);
+        }
+
+        public void MoveBy(float x, float y)
+        {
+            var newVertex = new PointF[VerticeCount];
+            for (int i = 0; i < Vertex.Length; i++)
+            {
+                var point = Vertex[i];
+                newVertex[i] = new PointF(point.X + x, point.Y + y);
+            }
+            Vertex = newVertex;
+        }
+
+        public void Rotate(float angle, PointF origin)
+        {
+            double angleInRadians = angle * (Math.PI / 180);
+            var sin = (float)Math.Sin(angle);
+            var cos = (float)Math.Cos(angle);
+            var newVertex = new PointF[VerticeCount];
+            for (int i = 0; i < Vertex.Length; i++)
+            {
+                newVertex[i] = new PointF
+                {
+                    X =
+            (float)
+            (cos * (Vertex[i].X - origin.X) -
+            sin * (Vertex[i].Y - origin.Y) + origin.X),
+                    Y =
+            (float)
+            (sin * (Vertex[i].X - origin.X) +
+            cos * (Vertex[i].Y - origin.Y) + origin.Y)
+                };
+            }
+            Vertex = newVertex;
+        }
+
+        static bool onSegment(PointF p, PointF q, PointF r)
+        {
+            if (q.X <= Math.Max(p.X, r.X) &&
+                q.X >= Math.Min(p.X, r.X) &&
+                q.Y <= Math.Max(p.Y, r.Y) &&
+                q.Y >= Math.Min(p.Y, r.Y))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static int orientation(PointF p, PointF q, PointF r)
+        {
+            float val = (q.Y - p.Y) * (r.X - q.X) -
+                    (q.X - p.X) * (r.Y - q.Y);
+
+            if (val == 0)
+            {
+                return 0;
+            }
+            return (val > 0) ? 1 : 2;
+        }
+
+        static bool doIntersect(PointF p1, PointF q1,
+                                PointF p2, PointF q2)
+        { 
+            int o1 = orientation(p1, q1, p2);
+            int o2 = orientation(p1, q1, q2);
+            int o3 = orientation(p2, q2, p1);
+            int o4 = orientation(p2, q2, q1);
+
+            if (o1 != o2 && o3 != o4)
+            {
+                return true;
+            }
+
+            if (o1 == 0 && onSegment(p1, p2, q1))
+            {
+                return true;
+            }
+
+            if (o2 == 0 && onSegment(p1, q2, q1))
+            {
+                return true;
+            }
+
+            if (o3 == 0 && onSegment(p2, p1, q2))
+            {
+                return true;
+            }
+
+            if (o4 == 0 && onSegment(p2, q1, q2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public static bool isInside(PointF[] polygon, int n, PointF p)
+        {
+            if (n < 3)
+            {
+                return false;
+            }
+
+            PointF extreme = new PointF((float)10000, p.Y);
+
+            int count = 0, i = 0;
+            do
+            {
+                int next = (i + 1) % n;
+
+                if (doIntersect(polygon[i],
+                                polygon[next], p, extreme))
+                {
+                    if (orientation(polygon[i], p, polygon[next]) == 0)
+                    {
+                        return onSegment(polygon[i], p,
+                                        polygon[next]);
+                    }
+                    count++;
+                }
+                i = next;
+            } while (i != 0);
+
+            return (count % 2 == 1);
+        }
+    }
+}
